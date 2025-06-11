a24be93300ffe54e1435da49b5b734b7df002fa6
Mohd Zaid <[razvizaid259@gmail.com]>
2025-06-11 08:02:14 +0530

    fixes herby

--- src/components/Herby.jsx
+++ src/components/Herby.jsx
@@ -5,6 +5,11 @@ function Herby() {
   const navigate=useNavigate()
   
   useEffect(()=>{
+    const script = document.createElement("script");
+    script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+    script.async = true;
+    document.body.appendChild(script);
+
     function exit(e){
       if(!e.target.className.includes('chat')){
         navigate('/')
@@ -15,19 +20,20 @@ function Herby() {
     
     return () => {
       document.removeEventListener('click',exit);
+      document.body.removeChild(script);
     };
   },[navigate])
   
   return (
     <div className="container h-dvh flex justify-center items-center">
-      <iframe
-        height='700'
-        width='700'
-        loading="lazy"
-        className="chat sm:rounded shadow-lg h-full sm:h-[500px]"
-        allow="microphone;"
-        src="//console.dialogflow.com/api-client/demo/embedded/0bb6ab00-08c3-4b83-a11e-9bf552797576"
-      ></iframe>
+      <df-messenger
+        chat-icon="https:&#x2F;&#x2F;cdn.asksid.ai&#x2F;himalaya&#x2F;Care_icon.svg"
+        intent="WELCOME"
+        chat-title="Herby"
+        agent-id="0bb6ab00-08c3-4b83-a11e-9bf552797576"
+        language-code="en"
+        className='chat sm:rounded shadow-lg h-full sm:h-[500px]'
+      ></df-messenger>
     </div>
   );
 }
