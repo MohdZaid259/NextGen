c282e13d88bea8cd243b97b54a39c368d7455aa4
Mohd Zaid <[razvizaid259@gmail.com]>
2025-06-11 08:32:27 +0530

    fixes herby

--- src/components/Herby.jsx
+++ src/components/Herby.jsx
@@ -5,43 +5,36 @@ function Herby() {
   const navigate=useNavigate()
   
   useEffect(()=>{
-    // const script = document.createElement("script");
-    // script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
-    // script.async = true;
-    // document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+    script.async = true;
+    document.body.appendChild(script);
 
-    function exit(e){
-      if(!e.target.className.includes('chat')){
-        navigate('/')
-      }
-    }
-    const container=document.querySelector('.container')
-    container.addEventListener('click',exit)
-    
+    // const exit = (e) => {
+    //   if(!e.target.className.includes('chat')){
+    //     navigate("/");
+    //   }
+    // }
+    // const container=document.querySelector('.container')
+    // container.addEventListener('click',exit)
+
     return () => {
-      document.removeEventListener('click',exit);
-      // document.body.removeChild(script);
+      document.body.removeChild(script);
+      // document.removeEventListener('click',exit);
     };
   },[navigate])
   
   return (
     <div className="container h-dvh flex justify-center items-center">
-      {/* <df-messenger
-        chat-icon="https:&#x2F;&#x2F;cdn.asksid.ai&#x2F;himalaya&#x2F;Care_icon.svg"
-        intent="WELCOME"
-        chat-title="Herby"
-        agent-id="0bb6ab00-08c3-4b83-a11e-9bf552797576"
-        language-code="en"
-        className='chat sm:rounded shadow-lg h-full sm:h-[500px]'
-      ></df-messenger> */}
-      <iframe
+        <df-messenger
+          chat-icon="https://cdn.asksid.ai/himalaya/Care_icon.svg"
+          intent="WELCOME"
+          chat-title="Herby"
+          agent-id="0bb6ab00-08c3-4b83-a11e-9bf552797576"
+          language-code="en"
           className='chat sm:rounded shadow-lg h-full sm:h-[500px]'
-          allow="microphone;"
-          width="350"
-          height="430"
-          src="https://console.dialogflow.com/api-client/demo/embedded/0bb6ab00-08c3-4b83-a11e-9bf552797576">
-      </iframe>
-    </div>
+        ></df-messenger>
+=    </div>
   );
 }
 
